87163be06c313edac2377ed30fe0cac674ef8015
Jacob Post <jacob.post.2020@student.lhschools.org>
2018-09-21 13:27:38 -0400

    Started coding
    Coding of the player class has started.

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Player {
+
+    //Turns could be handled by one takeTurn() method to encapsulate all of the player's options. After this method has
+    //run completely, the player's turn will automatically be terminated, voiding the need for a method to end the turn.
+
+    //Scanner PlayerNumber = new Scanner(System.in);
+
+//    public int numberOfPlayers() {
+//        System.out.println("Welcome to Yahtzee! Please enter the number of players that will be playing the game.");
+//
+//    }
+
+    private String findPlayerName;
+
+    //possibly do research over weekend and figure out how to set-up the code to be able to get the name of the players
+    //for use in the System.out.println(); that will print out a line saying which player's turn it is.
+
+
+   // String findPlayerName
+
+
+    public int takeTurn() {
+        System.out.println("");
+
+
+
+
+
+    }
 }
