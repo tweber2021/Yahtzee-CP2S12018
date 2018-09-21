6518e2dff788381781eb7407a3512553ac27c45b
Thomas <thomas.weber.2021@student.lhschools.org>
2018-09-21 13:27:38 -0400

    Working Die Class

--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -1,2 +1,9 @@
 public class Yahtzee {
+    public static void main(String[] args){
+        // Die test, nothing pertaining to the actual game yet
+        Die D = new Die();
+        System.out.println("Before reroll: "+D.getFaceValue());
+        D.rollDie();
+        System.out.println("After reroll: "+D.getFaceValue());
+    }
 }
