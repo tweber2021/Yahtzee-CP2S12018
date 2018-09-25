ea335c982d904f073d0d7e65d1c643d974296787
Austin <austin.thornton.2019@student.lhschools.org>
2018-09-25 13:26:34 -0400

    Players get new catagories

--- src/Rules.java
+++ src/Rules.java
@@ -1,35 +1,44 @@
 public class Rules {
 
+    private static int Playeramount;
     private boolean[] Scoring;
     private int totalScore;
     private int[] catagoryScore;
     private boolean[] catagoryCheck;
-
+    private Player player;
     //needs to get created for every player at the time that the player is created
 
-    public Rules() {
+    public Rules(Player player) {
+        this.player = player;
         catagoryCheck = new boolean[13];
         for (int i = 0; i < catagoryCheck.length; i++) {
             catagoryCheck[i] = false;
 
         }
-        totalScore = 0;
+
+        for (Player player) {
         catagoryScore = new int[13];
         for (int i = 0; i < catagoryScore.length; i++) {
             catagoryScore[i] = 0;
-
+        }
         }
     }
 
-    public int checkScore(Die[] dice) {
+    public int checkScore(player.Die[] dice) {
         for (int i = 0; i < this.catagoryCheck.length; i++) {
             if (catagoryCheck[i]) {
                 return dice;
             } else if (checkAces()) {
+                catagoryScore[] = 
+            }
+
+            public int checkDie(int faceValue) {
+                for (faceValue = player.Die[]);
 
             }
+
         }
-    return dice;
+    return Die[];
     }
 
 
