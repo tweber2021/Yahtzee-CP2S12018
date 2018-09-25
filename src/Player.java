3997e39bb9a309f4894672f625420b5f20ce29e8
Thomas <thomas.weber.2021@student.lhschools.org>
2018-09-25 13:27:04 -0400

    Added determining a winner after game ends

--- src/Player.java
+++ src/Player.java
@@ -10,11 +10,11 @@ public class Player { // NOT REAL Player class - for demo purposes only
     public void takeTurn(){
         try{Thread.sleep(1000);} // Pretend to take a turn
         catch(Exception e){} // Handle possible exception
-        System.out.println("Player "+this.name+" has taken their turn.");
+        System.out.println(this.name+" has taken their turn.");
         this.finalScore += (int)(Math.floor(Math.random()*10));
-        if(Math.random()>0.8){ // 20% chance to stop playing randomly
+        if(Math.random()>0.9){ // 20% chance to stop playing randomly
             isPlaying=false;
-            System.out.println("Player "+this.name+" has stopped playing.");
+            System.out.println(this.name+" has stopped playing.");
         }
     }
 
