f6fccc8695f11f3a27488685463c8020c25530ff
Austin <austin.thornton.2019@student.lhschools.org>
2018-10-10 14:09:48 -0400

    Checking Upper complete, All boolean categories complete
    All upper checks are done, all categories are done. Lower section will be done for initial go.

--- src/Rules.java
+++ src/Rules.java
@@ -6,107 +6,212 @@ public class Rules {
     private boolean[] categoryCheck;
     private Player player;
 
-    //needs to get created for every player at the time that the player is created
 
-
+    // Player class creates new player in here
     public Rules(Player player) {
+
+
+        // Getting whichever players going
         this.player = player;
+
+
+        // Created 13 true or false categories
         categoryCheck = new boolean[13];
-        for (int i = 0; i < categoryCheck.length; i++) {
+
+/*
+
+        // length i is starting at 0, Category 1 would be 0, and so on.
+        for (int i = 0; i < categoryCheck.length;i++) {
+            // Counting up to .length = [13]
+            // Since i less than, will start 0-12, i is not <= so we -1 from regular categories to match them
+            // When returned false and not true, then the category is available for this.player to use
             categoryCheck[i] = false;
-
         }
+*/
 
-        for (Player player) {
+
+
+
+
+            // Creating 13 default 0 categories with categoryScore holding dice values
             categoryScore = new int[13];
+            // Just like categoryCheck but with int, 0-12 , 13 available numbers
             for (int i = 0; i < categoryScore.length; i++) {
+
+
+                // This is where the players score starts at, 0 for default
+                // If a player scores a 0 it will count as a new score and flip the boolean from false to true
                 categoryScore[i] = 0;
             }
-        }
+
     }
 
-    //what score is in that catagory
-    public int checkScore(Die[] dice) {
+
+
+
+    public boolean checkCategoryInput(int chose) {
+        if(checkCategory(chose-1)){
+            if(chose<7 && chose>0){
+                if(checkUpper(player.getDice(),chose)){
+                    categoryScore[chose-1] = sumDice(player.getDice(),chose);
+                    categoryCheck[chose-1] = true;
+                    return true;
+                }
+            }
+            else{
+                //todo lowersection
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private boolean checkUpper(Die[] dice, int value) {
+        for (Die mFaceValue: dice) {
+            if (mFaceValue.getFaceValue() == value) {
+                return true;
+            }}
+        return false;
+    }
+
+    public boolean checkCategory(int pValue) {
+        int fixedNumber;
+        if (pValue < 1 || pValue > 12) {
+            return false;
+        } else {
+            return categoryCheck[pValue];
+        }
+
+
+
+    }
+
+    private int sumDice(Die[] dice, int value){
+
+
+        // The subtotal will be 0, until returned with the new faceValue
+        // We want to grab all dice and check them as well, not only score them
+        int subTotal = 0;
+
+        // facevalue is getting the dice now
+        for(Die die: dice){
+            int facevalue = die.getFaceValue();
+
+            // If facevalue equals to the value then subtotal will be returnable as long as there is a matched int
+            if(facevalue == value){
+
+
+                // subTotal will be greater or equals to faceValue then
+                subTotal += facevalue;
+            }
+        }
+
+
+        // subTotal is switched from 0 to the new score of the 5 dices
+        return subTotal;
+
+
+    }
+
+
+    /*// Back to category check, when a category is checked by a successful method, we will return it to true.
+    public int checkScore(int[] dice) {
+
+
+        // Now getting this. (certain category, looks into each one until it finds categoryCheck[0-12]
+        // Will check the score if 0 return the dice to the player and either end or next turn created by Player
         for (int i = 0; i < this.categoryCheck.length; i++) {
+
+
+            // if category is false categoryCheck[i=false]      { return dice; to player
             if (categoryCheck[i]) {
+
+
+                // Now giving the player the dice back since they cant do anything with it
                 return dice;
-            } else if (checkAces()) {
-                Player.Die[] =categoryScore[1];
-            } else if (checkTwos()) {
 
+
+                // This is part 1 of 2 of checking, with this part were grabbing the dice for whichever upper/lower section they selected
+                // Once it goes throughout the code for the upper/lower sectioned game rules code, and if they scored an actual score
+                // Then we will flip the category from false to true for the selection they chose
+
+
+                // ------ This section still needs tweaked and fixed a little ------
+            } else if (checkUpper()) {
+
+
+                // Category check is now switched to true
+                categoryCheck[0] = true;
+
+
+                // Category Score is done in the part 2 of 2, which is called checkDie
+
+                // Doing the same thing as checkUpper but for checking checkTwos die requirements
+                // Will be done during beta
             }
+
+            }}
+            return dice;
+
         }
+*/
 
-    }
-        private int sumDice(int[] dice, int value){
-            int subTotal = 0;
-            for(int facevalue: dice){
-               if(facevalue == value){
-                   subTotal += facevalue;
-               }
-            }
-            return subTotal;
-        }
 
-        public int checkDie (int[] dice){
 
+    // Getting int[0-12] and a new value to add up the (sub) total, not initial score (These are where the 5 dices are placed and checked)
+
+
+
+
+
+    //Checking the real score from the dice values
+   /*  public void checkDieRealScore (Die[] dice){
+
+
+        // Created new diceValue now is this.players score, then getting the score to return to diceValue
+        int NewDvalue = this.player.setScore();
         int diceValue = this.player.getScore();
-           if(checkAces(dice)){
-              diceValue += sumDice(dice, 1);
-              this.player.setScore = diceValue;
-           } else if (checkTwos(dice)) {
-           diceValue += sumDice(dice, 2);
-           this.player.setScore = diceValue;
-           }
+
+        // now checking part 2 of 2 of upper section and lower later, getting the dice score which whatever value is needed
+        for(int i = 1; i<=6;i++){
+        if (checkUpper(dice, i)) {
 
 
-        return diceValue;
-    }
+            //Trying to get the 5 dice and
+            diceValue += sumDice(dice,i);
+            // diceValue now equals the amount of dices that equalled a one, so 1-5, and this score will be set into the categoryScore[0] later
+
+            //New ]]
+            //NewDvalue += totalDice()
 
 
-    private boolean checkAces(Die[] dice) {
 
-                for(int faceValue: dice){
-                    if(faceValue. ==1){
-                        return true;
-                    } }
-                return false;
-            } private boolean checkTwos(int[] dice) {
-        for (int faceValue : dice) {
-            if (faceValue == 2) {
-                return true;
-            } }
-        return false;
-    } private boolean checkThrees(int[] dice) {
-        for(int faceValue: dice){
-            if(faceValue == 3){
-                return true;
-            } }
-        return false;
-    } private boolean checkFours(int[] dice) {
-        for(int faceValue: dice){
-            if(faceValue == 4){
-                return true;
-            } }
-        return false;
-    } private boolean checkFives(int[] dice) {
-        for(int faceValue: dice){
-            if(faceValue == 5){
-                return true;
-            } }
-        return false;
-    } private boolean checkSixes(int[] dice) {
-        for(int faceValue: dice){
-            if(faceValue == 6){
-                return true;
-            } }
-        return false;
-    }
+        }
+        }
+
+        this.player.setScore(diceValue);
+
+        // Gets the new diceValue depending on which check method was chosen and how many dice landed or scored that
+
+    }*/
+
+
+        // mFaceValue is a temporary hold for holding class Die's Die array
+
+
 
     //Lower section will be worked on during beta
 
 
+    public void calculatePlayerScore(){
+        int subtotal = 0;
+        for(int value: categoryScore){
+            subtotal += value;
+        }
+        totalScore = subtotal;
 
+    }
 
 
 
@@ -116,12 +221,12 @@ public class Rules {
         return Scoring;
     }
 
-    // This obvoiusly cant be done until atleast beta because alpha will not have lower section complete yet to add up
-    // this is aviable for any class so its set public if you need it for main
+    // This obviously cant be done until atleast beta because alpha will not have lower section complete yet to add up
+    // this is available for any class so its set public if you need it for main
     public int getTotalScore() {
-        getTotalScore(Player player) = totalScore;
-        totalScore =
+        return totalScore;
 
     }
 
 }
+
